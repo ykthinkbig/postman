c48cc49da3fb26edd613a2a602ead47f79341a83
rhydwharn <ridwanconcept@yahoo.com>
2020-03-21 19:22:18 +0100

    push first code commit

---
+++ src/test/java/Login/LoginTest.java
@@ -0,0 +1,30 @@
+package Login;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class LoginTest {
+
+    private WebDriver driver;
+    public void setUp() throws InterruptedException {
+        System.setProperty("webdriver.chrome.driver", "resources/chromedriver.exe");
+        driver = new ChromeDriver();
+        driver.get("https://www.postman.com/");
+        Thread.sleep(5000);
+        driver.manage().window().maximize();
+        System.out.println(driver.getTitle());
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+        driver.findElement(By.linkText("Sign In")).click();
+        driver.findElement(By.name("username")).sendKeys("jideluvs@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("L0rlah10.");
+        driver.findElement(By.xpath("//button[@id='sign-in-btn']")).click();
+    }
+
+    public static void main(String args[]) throws InterruptedException {
+        LoginTest test = new LoginTest();
+        test.setUp();
+    }
+}
